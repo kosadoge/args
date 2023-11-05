2d39cb3e744ebe5370409b9f2db046ae71f3b21b
KosaDoge <deadlyinu@gmail.com>
2023-11-05 19:54:22 +0800

    docs: Add example for using a JSON configuration file

--- example_test.go
+++ example_test.go
@@ -44,3 +44,28 @@ func Example_withEnvironments() {
 	// port: 8888
 	// debug: true
 }
+
+func Example_withConfigFile() {
+	// $ cat ./testdata/json_config.json
+	// {
+	// 	"port": "8888",
+	// 	"debug": true,
+	// 	"log_level": "info"
+	// }
+	// $ app --config ./testdata/json_config.json
+	os.Args = []string{"app", "--config", "./testdata/json_config.json"}
+
+	fs := args.New()
+	var (
+		port   = fs.String("port,p", "9999", "listen port")
+		debug  = fs.Bool("debug", false, "enable debug mode")
+		config = fs.String("config", "", "config file")
+	)
+	fs.Parse(os.Args[1:], args.Json(config))
+
+	fmt.Println("port:", *port)
+	fmt.Println("debug:", *debug)
+	// Output:
+	// port: 8888
+	// debug: true
+}
